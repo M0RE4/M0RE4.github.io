71a5e8c104f4cbddc5c4d87491b30f8743b6b46b
magnus <magnushdalgaard@gmail.com>
2022-09-28 08:17:34 +0200

    ny måde at tælle ned på afprøvet

--- Python/Løkker/10 til 1.py
+++ Python/Løkker/10 til 1.py
@@ -1,13 +1,5 @@
-import time
-
-def countdown(time_sec):
-    while time_sec:
-        mins, secs = divmod(time, 60)
-        timeformat = '{:02d}:{:02d}'.format(mins, secs)
-        print(timeforamt, end='\r')
-        time.sleep(1)
-        time_sec -= 1
-
-        print("stop")
-
-    countdown(5)
+    //how to make a code that counts from 10 to 1?
+count = 1
+while count <= 100:
+    print(count)
+    count += 1
