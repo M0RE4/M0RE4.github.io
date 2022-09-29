412066257c064e7a380e2cc82e6e9bf4e8a4c047
magnus <magnushdalgaard@gmail.com>
2022-09-29 16:36:08 +0200

    lavet ligninger 0_100 og ligninger 0_10 færdige

--- Python/Løkker/ligning.py
+++
@@ -1,6 +0,0 @@
-print('for y=3x^2 + 6x + 9 så er y i lig med = ')
-for x in range(0,11):
-y=3*x**2+6*x+9
-print('')
-print('når x= '+ str(x)+' er:')
-print('y=' + str(y))
---
+++ Python/Løkker/ligning_0_100.py
@@ -0,0 +1,6 @@
+
+for x in range(0,101,10):
+    y=3*x**2+6*x+9
+    print('')
+    print('når x= '+ str(x)+' er:')
+    print('y=' + str(y))
