72752d01d50481e018dad4e05647b05faea8837b
M0RE4 <MagnusHDalgaard@gmail.com>
2023-01-19 13:22:34 +0100

    yes

--- Python/test.py
+++ Python/test.py
@@ -1,13 +1,5 @@
-# Find the next even number
-
-def find_next_even(num):
-  # If the number is even, add 2 to get the next even number
-  if num % 2 == 0:
-    return num + 2
-  # If the number is odd, add 1 to get the next even number
-  else:
-    return num + 1
-
-# Test the function
-print(find_next_even(5))  # should print 6
-print(find_next_even(12))  # should print 14
+print("   __")
+print(" // \\\\")
+print("| 0 0 |")
+print(" \\ __ /")
+print("   ||")
